0582406df04142bbd2d36e626aae26429093fc50
Lucas Becker <lucasbecker451@gmail.com>
2022-08-05 23:56:51 -0400

    set up states and error checking

--- addons/smart-graphics-settings/SmartGraphicsSettings.gd
+++ addons/smart-graphics-settings/SmartGraphicsSettings.gd
@@ -1,7 +1,7 @@
 extends Node
 class_name SmartGraphicsSettings, "res://addons/smart-graphics-settings/GodotSmartGraphicsSettings.svg"
 
-export(int, 0, 500) var target_fps := 60
+export(int, 0, 500) var target_fps := 60 setget set_target_fps
 export(Environment) var environment: Environment = ProjectSettings.get(
 	"rendering/environment/default_environment"
 )
@@ -26,7 +26,7 @@ onready var _semaphore := Semaphore.new()
 onready var _thread := Thread.new()
 onready var _check_timer := Timer.new()
 onready var state := _validate_export_vars()
-onready var thread_mode := _get_thread_mode()
+onready var thread_mode: int = ThreadMode.ERROR
 
 const _MIN_TARGET_FPS := 0
 const _MAX_TARGET_FPS := 1000
@@ -41,6 +41,18 @@ func disable() -> void:
 	_enabled = false
 	set_process(false)
 
+func is_enabled() -> bool:
+	return _enabled
+
+func set_target_fps(new_target_fps: int = 60) -> void:
+	if new_target_fps < _MIN_TARGET_FPS or new_target_fps > _MAX_TARGET_FPS:
+		printerr("New target FPS is out of range.",
+		"\n_MIN_TARGET_FPS: ", _MIN_TARGET_FPS, 
+		"\n_MAX_TARGET_FPS: ", _MAX_TARGET_FPS, 
+		"\nnew_target_fps: ", new_target_fps)
+		print_stack()
+		return
+	target_fps = new_target_fps
 
 func _ready() -> void:
 	if state == State.READY:
@@ -49,6 +61,7 @@ func _ready() -> void:
 		disable()
 		printerr("SmartGraphicsSettings encountered an error and could not be initialized.")
 		print_stack()
+		return
 	print_debug("target_fps: ", target_fps, "\nenvironment: ", environment)  # TODO: remove this debug print
 
 
@@ -58,7 +71,10 @@ func _process(delta: float) -> void:
 
 func _validate_export_vars() -> int:
 	if target_fps < _MIN_TARGET_FPS or target_fps > _MAX_TARGET_FPS:
-		printerr("Target FPS is out of range.\ntarget_fps: ", target_fps)
+		printerr("New target FPS is out of range.",
+		"\n_MIN_TARGET_FPS: ", _MIN_TARGET_FPS, 
+		"\n_MAX_TARGET_FPS: ", _MAX_TARGET_FPS, 
+		"\ntarget_fps: ", target_fps)
 		print_stack()
 		return State.ERROR
 	if not environment:
@@ -68,13 +84,21 @@ func _validate_export_vars() -> int:
 	return State.READY
 
 
-func _get_thread_mode(new_thread_mode: int = -1) -> int:
-	if new_thread_mode != -1:
-		thread_mode = new_thread_mode
-		return thread_mode
+func set_thread_mode(new_thread_mode: int = -1) -> void:
+	if new_thread_mode in ThreadMode.values():
+		if new_thread_mode == ThreadMode.THREADED:
+			if OS.can_use_threads():
+				thread_mode = new_thread_mode
+			else:
+				printerr("SmartGraphicsSettings cannot run in multi-threaded mode on this OS: ", 
+				OS.get_name(), "\nDefaulting to single thread mode.")
+				thread_mode = ThreadMode.SINGLE
+		else:
+			thread_mode = new_thread_mode
 	elif OS.can_use_threads():
-		return ThreadMode.THREADED
-	return ThreadMode.SINGLE
+		thread_mode = ThreadMode.THREADED
+	else:
+		thread_mode = ThreadMode.SINGLE
 
 
 func _check() -> int:
