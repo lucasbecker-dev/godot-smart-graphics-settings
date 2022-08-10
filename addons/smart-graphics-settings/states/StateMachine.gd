62fc471c1296749376d839eb7ec5e314dc1163ed
Lucas Becker <lucasbecker451@gmail.com>
2022-08-10 16:26:01 -0400

    finish StateMachine essentials

--- addons/smart-graphics-settings/states/StateMachine.gd
+++ addons/smart-graphics-settings/states/StateMachine.gd
@@ -1,22 +1,39 @@
 extends Resource
 
-var _states: Array setget set_states, get_states
+var _states: Array setget , get_states
 var _current_state: Resource setget set_current_state, get_current_state
 var _state_class := preload("res://addons/smart-graphics-settings/states/State.gd")
 
-signal state_changed(new_state)
+signal states_list_changed(states_list)
+signal current_state_changed(current_state)
 
 
-func _init(states: Array) -> void:
-	set_states(states)
+func _init(states: Array, current_state: Resource) -> void:
+	set_states(states, current_state)
 
 
 func process_execute() -> void:
-	_current_state.process_execute
+	if _current_state_exists():
+		_current_state.process_execute
+	else:
+		printerr(
+			"ERROR: StateMachine ",
+			self,
+			" does not have a current state, and thus cannot execute process logic. Set one before use with set_current_state()."
+		)
+		print_stack()
 
 
 func physics_process_execute() -> void:
-	_current_state.physics_process_execute
+	if _current_state_exists():
+		_current_state.physics_process_execute
+	else:
+		printerr(
+			"ERROR: StateMachine ",
+			self,
+			" does not have a current state, and thus cannot execute physics process logic. Set one before use with set_current_state()."
+		)
+		print_stack()
 
 
 func set_states(states: Array, current_state: Resource) -> void:
@@ -46,7 +63,7 @@ func add_state(state: Resource, is_current_state: bool = false) -> void:
 		return
 	_states.push_back(state)
 	if is_current_state:
-		_current_state = _states.back()
+		set_current_state(state)
 
 
 func remove_state(state: Resource) -> void:
@@ -58,11 +75,24 @@ func remove_state(state: Resource) -> void:
 		printerr("ERROR: State ", state, " was not found in in StateMachine ", self)
 		print_stack()
 		return
+	if state == _current_state:
+		set_current_state(null)
+		printerr(
+			"ERROR: Removed state ",
+			state,
+			" from StateMachine ",
+			self,
+			", which was the StateMachine's current state."
+		)
 	while state in _states:
 		_states.erase(state)
 
 
 func set_current_state(state: Resource) -> void:
+	if state == null:
+		_current_state = null
+		emit_signal("state_changed", null)
+		return
 	if !(state is _state_class):
 		printerr("ERROR: ", state, " is not a State.")
 		print_stack()
@@ -72,6 +102,7 @@ func set_current_state(state: Resource) -> void:
 		print_stack()
 		return
 	_current_state = state
+	emit_signal("state_changed", _current_state)
 
 
 func get_current_state() -> Resource:
